f51063cdbc654a9100d2884a5a9e3b6651f57abc
hui0309 <abbyhuang20020309@gmail.com>
2024-04-22 06:17:54 +0800

    add comment json

--- getcomment.py
+++ getcomment.py
@@ -11,8 +11,6 @@ chromedriver = '/usr/bin/chromedriver'
 
 # Create an instance of Options
 options = Options()
-# options.add_argument('--headless')
-# options.add_argument('--user-agent=%s' % user_agent)
 
 # Add any desired options here, for example:
 # options.add_argument("--headless")  # Run Chrome in headless mode
@@ -26,41 +24,15 @@ driver = webdriver.Chrome(service=service, options=options)
 
 # driver open http---------------------------------------------------------------------
 driver.get('https://play.google.com/store/apps/details?id=com.gamania.lineagem&hl=zh-TW')
-# # print(pageSource)
-# # # 關閉瀏覽器
-# driver.close() 
 
-# def click_text(obj):
-# #尋找「查看所有評論」的字串並點擊
-#     try:
-#         obj.find_element_by_xpath("//span[contains(text(),'查看所有評論')]").click()       
-#     except:
-#         pass
-
-# move = driver.find_element_by_tag_name('body')
-# click_text(move)
 
 # 點擊查看所有評論
-# sees = driver.find_element(by=By.CSS_SELECTOR, value="button")
-# see = driver.find_element(by = By.CSS_SELECTOR, value = 'span[class="VfPpkd-vQzf8d"][jsname="V67aGc"][text()="查看所有評論"]')
 see = driver.find_element(by = By.XPATH, value = '//c-wiz/section/div/div/div/div/div/button')
 see.click()
-
-# reviews = driver.find_element(by = By.CSS_SELECTOR, value = 'div[class='']')
-
-# # 取得網頁原始碼
-# driver.find_element_by_xpath("//span[contains(text(),'查看所有評論')]").click() 
-# # for ranking in rankings:
-#     if rankings.aria-label == "營收最高的項目":
-#         ranking.click()   # 點擊登入按鈕d
-#         print("切換到營收最高的項目")
-# D3Qfie VfPpkd-ksKsZd-XxIAqe kofMvc EFMXQ VfPpkd-ksKsZd-mWPk3d
 
 import requests
 from bs4 import BeautifulSoup
 
-# # # # tag = input("請輸入定位元素，class前面加上.，id前面加上# ")
-# # # res = requests.get('https://play.google.com/store/games?hl=zh_TW&gl=US')
 for i in range(50):
     iframe = driver.find_element(By.CSS_SELECTOR, "div[class='odk6He']")
     scroll_origin = ScrollOrigin.from_element(iframe)
@@ -94,6 +66,9 @@ for result in results:
     
     # Add the dictionary to the list
     post_details.append(post_dict)
+
+# 關閉瀏覽器
+# driver.close() 
 
 print(len(post_details))
 
