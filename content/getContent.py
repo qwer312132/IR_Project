2c1c3d333978894c2a5ecf026169105d31cc0c27
qwer312132 <roony20020505@gmail.com>
2024-05-10 10:31:39 +0800

    add genere field in gameContent

--- content/getContent.py
+++ content/getContent.py
@@ -1,4 +1,4 @@
-from google_play_scraper import app, reviews
+from google_play_scraper import app
 import json
 def remove_prefix_and_suffix(string):
     prefix = "https://app.sensortower.com/overview/"
@@ -11,18 +11,22 @@ for line in f:
     gameId.append(remove_prefix_and_suffix(line.strip()))
 f.close()
 gameId = list(set(gameId))
-# print(len(gameId))
-# exit()
 data = []
-review_count = 100
-for ID in gameId:
+for i in range(len(gameId)):
+    ID = gameId[i]
+    print(i, ID)
     # print(ID)
     game_data = {}
-    app_info = app(ID, lang='zh_TW', country='TW')
-    game_data['ID'] = ID
-    game_data['title'] = app_info['title']
-    game_data["description"] = app_info["description"]
-    data.append(game_data)
+    try:
+        app_info = app(ID, lang='zh_TW', country='TW')
+        game_data['ID'] = ID
+        game_data['title'] = app_info['title']
+        game_data["description"] = app_info["description"]
+        game_data["genre"] = app_info["genre"]
+        data.append(game_data)
+    except:
+        continue
 # print(data)
+print(len(data))
 with open('gameContent.json', 'w', encoding='utf8') as f:
     json.dump(data, f, ensure_ascii=False)
